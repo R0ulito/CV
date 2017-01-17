46cfca3de4f49aa3c00c24573698d53ee07877a5
R0ulito <roulit77@gmail.com>
2017-01-17 23:58:44 +0100

    Changements fais sur page CV et page index

--- cv.php
+++ cv.php
@@ -28,7 +28,9 @@
 		Permis B <br>
 		0662478251 <br>
 		julien.jovy@gmail.com <br>
-		Github: R0ulito
+		Github: <a target="_blank" href="https://github.com/R0ulito">R0ulito</a>
+        <br>
+        Linkedin: <a target="_blank" href="https://fr.linkedin.com/in/julien-jovy-502455121">https://fr.linkedin.com/in/julien-jovy</a>
 		</p>		
 	</div>
 		<div>
@@ -128,14 +130,14 @@
 				</div>
 
 				Symphony
-				<div class="progress">
-					<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%"> In progress
+				<div class="progress active">
+					<div class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%"> In progress
 					</div>
 				</div>
 
 				Wordpress
-				<div class="progress">
-					<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%"> In progress
+				<div class="progress active">
+					<div class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%"> In progress
 					</div>
 				</div>
 
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
-	<title>Portfolio HTML+CSS</title>
+	<title>Portfolio HTML+CSS - Framework Bootstrap</title>
 <meta charset="UTF-8">
 </head>
 <body>
@@ -15,14 +15,26 @@
 		<div class="jumbotron col-xs-offset-2 col-xs-8 thumbnail" style="border:2px outset silver;">
 			<h2 class="text-capitalize col-lg-offset-1">à propos</h2>
 			<p class="col-xs-offset-1">
-				Bonjour ! <br> 
-				Je m'appelle <strong>Julien Jovy</strong>.<br>
+				<br>
+				Bonjour !
+				<br>
+				<br>
+				Je m'appelle <strong>Julien</strong>.
+				<br>
+				<br>
 				Passionné depuis toujours par l'informatique, je me suis inscrit au cursus de Technicien d'Assistance Informatique
 				au GRETA de Lognes (77) en septembre 2015.<br>
+				J'en suis sorti major de promotion, et j'ai obtenu le titre professionnel avec les félicitations du jury.
+				<br> <br>
 				Au sein de ce centre j'ai pris en compétences en
 				<strong>Réseau</strong>, en
 				<strong>Système</strong> et en assistance utilisateur.<br>
-				J'ai également pu affirmer mon projet de devenir développeur, avec une vraie préférence pour <strong>les langages web, le framework Bootstrap et python.</strong>
+				J'ai également pu affirmer mon projet de devenir développeur, avec une vraie préférence pour <strong>
+				<br>
+				les langages web, le framework Bootstrap et python.</strong>
+				<br>
+				<br>
+				Je suis actuellement technicien informatique au sein du lycée Gaston Bachelard de Chelles.
 				
 
 			</p>		
